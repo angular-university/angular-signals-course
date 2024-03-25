0eacb6bc8536900439693b8f5ecdc424da1607c5
vasco <jhades.dev@gmail.com>
2024-03-25 13:42:34 +0100

    angular signals course

---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-card-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses-card-list.component.html',
+  styleUrl: './courses-card-list.component.scss'
+})
+export class CoursesCardListComponent {
+
+}
