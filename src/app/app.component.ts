fe3ac385f4c3853f815b87e4219fc5a1108b9ea2
vasco <jhades.dev@gmail.com>
2024-03-25 10:40:37 +0100

    top menu skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, viewChild} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavContainer, MatSidenav, MatNavList, MatListItem, MatIcon, RouterLink, MatToolbar, MatIconButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-signals-course';
+
+  logout() {
+
+  }
+
 }
