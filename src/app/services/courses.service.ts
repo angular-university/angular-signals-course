a94a716230d3fd6dfb11a8edf204f691b8726167
vasco <jhades.dev@gmail.com>
2024-03-26 16:22:57 +0100

    angular signals course

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpContext} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {firstValueFrom} from "rxjs";
 import {Course} from "../models/course.model";
 import {GetCoursesResponse} from "../models/get-courses.response";
+import {SkipLoading} from "../loading/skip-loading.component";
 
 
 @Injectable({
@@ -21,7 +22,9 @@ export class CoursesService {
 
     console.log(`Calling API to load all courses...`);
 
-    const courses$ = this.http.get<GetCoursesResponse>(`${this.env.apiRoot}/courses`);
+    const courses$ = this.http.get<GetCoursesResponse>(`${this.env.apiRoot}/courses`, {
+      //context:  new HttpContext().set(SkipLoading, true),
+    });
 
     const response = await firstValueFrom(courses$);
 
