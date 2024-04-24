cc7e624ce58b235c9d4ee062ad1cabcd1e8c5c32
vasco <jhades.dev@gmail.com>
2024-04-24 13:03:27 +0200

    recordings

--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -10,10 +10,39 @@ export class CoursesServiceWithFetch {
 
   env = environment;
 
-  async loadAllCourses():Promise<Course[]> {
+  async loadAllCourses(): Promise<Course[]> {
     const response = await fetch(`${this.env.apiRoot}/courses`);
     const payload = await response.json();
     return payload.courses;
+  }
+
+  async createCourse(course: Partial<Course>): Promise<Course> {
+    const response = await fetch(`${this.env.apiRoot}/courses`, {
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify(course)
+    })
+    return await response.json();
+  }
+
+  async saveCourse(courseId: string,
+                   changes: Partial<Course>): Promise<Course> {
+    const response = await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify(changes)
+    });
+    return await response.json();
+  }
+
+  async deleteCourse(courseId:string):Promise<void> {
+    await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: "DELETE"
+    })
   }
 
 }
