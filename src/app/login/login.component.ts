448bcaa08d70dfa84ebf3ee19e35b1c4d43542b5
vasco <jhades.dev@gmail.com>
2024-04-30 12:48:21 +0200

    recordings

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,5 +16,38 @@ import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 })
 export class LoginComponent {
 
+  fb = inject(FormBuilder);
 
+  form = this.fb.group({
+    email: [''],
+    password: ['']
+  });
+
+  messagesService = inject(MessagesService);
+
+  authService = inject(AuthService);
+
+  router = inject(Router);
+
+  async onLogin() {
+    try {
+      const {email, password} = this.form.value;
+      if (!email || !password) {
+        this.messagesService.showMessage(
+          "Enter an email and password.",
+          "error"
+        )
+        return;
+      }
+      await this.authService.login(email, password);
+      await this.router.navigate(['/home']);
+    }
+    catch(err) {
+      console.error(err);
+      this.messagesService.showMessage(
+        "Login failed, please try again",
+        "error"
+      )
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import {computed, effect, inject, Injectable, signal} from "@angular/core";
 import {User} from "../models/user.model";
 import {environment} from "../../environments/environment";
 import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom} from "rxjs";
 
 const USER_STORAGE_KEY = 'user';
 
@@ -10,5 +12,21 @@ const USER_STORAGE_KEY = 'user';
 })
 export class AuthService {
 
+  #userSignal = signal<User | null>(null);
+
+  user = this.#userSignal.asReadonly();
+
+  isLoggedIn = computed(() => !!this.user());
+
+  http = inject(HttpClient);
+
+  async login(email:string, password:string): Promise<User> {
+    const login$ = this.http.post<User>(`${environment.apiRoot}/login`, {
+      email,
+      password});
+    const user = await firstValueFrom(login$);
+    this.#userSignal.set(user);
+    return user;
+  }
 
 }
