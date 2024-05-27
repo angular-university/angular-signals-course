e3dfe5dd1e6fa7626a944ee1daf79d1d909761af
vasco <jhades.dev@gmail.com>
2024-05-27 16:09:49 +0200

    upgrade to Angular 18

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {firstValueFrom} from "rxjs";
 import {Course} from "../models/course.model";
