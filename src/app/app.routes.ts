f182d88e62e06838ba1dda17793f21218a0d7d5c
vasco <jhades.dev@gmail.com>
2024-05-28 14:28:22 +0200

    recordings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,17 @@ import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {LessonsComponent} from "./lessons/lessons.component";
 import {isUserAuthenticated} from "./guards/auth.guard";
+import {CourseComponent} from "./course/course.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     canActivate: [isUserAuthenticated]
+  },
+  {
+    'path': 'courses/:courseId',
+    component: CourseComponent
   },
   {
     path: "login",
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {Course} from "../models/course.model";
+import {Lesson} from "../models/lesson.model";
 
 @Component({
   selector: 'course',
@@ -8,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './course.component.scss'
 })
 export class CourseComponent {
+
+  course = signal<Course | null>(null);
+
+  lessons = signal<Lesson[]>([]);
 
 }
