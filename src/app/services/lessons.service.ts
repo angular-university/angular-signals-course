591b58d540adc658e75ca006f4522c8bc2f7a100
vasco <jhades.dev@gmail.com>
2024-05-28 15:28:14 +0200

    recordings

--- src/app/services/lessons.service.ts
+++ src/app/services/lessons.service.ts
@@ -13,4 +13,28 @@ export class LessonsService {
 
   env = environment;
 
+  http = inject(HttpClient);
+
+  async loadLessons(config: {
+    courseId?:string,
+    query?:string;
+  }): Promise<Lesson[]> {
+    const {courseId, query} = config;
+    let params = new HttpParams();
+    if (courseId) {
+      params = params.set("courseId", courseId);
+    }
+    if (query) {
+      params = params.set("query", query);
+    }
+    const lessons$ = this.http.get<GetLessonsResponse>(
+      `${this.env.apiRoot}/search-lessons`,
+      {
+        params
+      }
+    )
+    const response = await firstValueFrom(lessons$);
+    return response.lessons;
+  }
+
 }
