e81d636429938b6e89715cd0b6669d21c844310a
hoseasandstrom <hosea.sandstrom@interloopdata.com>
2024-07-17 15:05:06 -0400

    Initial Commit
    Basic signal functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,56 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  signal,
+} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import {
+  toObservable,
+  toSignal,
+  outputToObservable,
+  outputFromObservable,
+} from '@angular/core/rxjs-interop';
+
+type Counter = {
+  value: number;
+};
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [
-    MatTabGroup,
-    MatTab,
-    CoursesCardListComponent
-  ],
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  counter = signal<Counter>({
+    value: 100,
+  });
 
+  values = signal<number[]>([0]);
+
+  append() {
+    this.values.update((values) => [...values, values[values.length - 1] + 1]);
+  }
+
+  increment() {
+    // this.counter++;
+    // this.counter.set(this.counter() + 1);
+    // this.counter.update((counter) => counter + 1);
+
+    //DO NOT MUATATE THE VALUE DIRECTLY; EMIT A NEW VALUE
+    this.counter.update((counter) => ({
+      ...counter,
+      value: counter.value + 1,
+    }));
+  }
 }
