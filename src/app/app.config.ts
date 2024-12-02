e221b323689cec62cfdff513fee58f3d88366dc2
vasco <jhades.dev@gmail.com>
2024-12-02 16:29:44 +0100

    new lessons Angular 19

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,7 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withFetch(),
       withInterceptors([
-        loadingInterceptor
+        //loadingInterceptor
       ])
     )
   ]
--- src/app/resource-demo/resource-demo.component.ts
+++ src/app/resource-demo/resource-demo.component.ts
@@ -1,13 +1,45 @@
-import {Component} from "@angular/core";
+import {Component, effect, inject, resource, signal} from "@angular/core";
+import {CoursesService} from "../services/courses.service";
+import {Course} from "../models/course.model";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 
 @Component({
   selector: 'resource-demo',
   templateUrl: './resource-demo.component.html',
-  styleUrls: ['./resource-demo.component.scss']
+  styleUrls: ['./resource-demo.component.scss'],
+  imports: [MatProgressSpinner]
 })
 export class ResourceDemoComponent {
 
+  coursesService = inject(CoursesService);
+
+  courseId = signal<string | null>(null);
+
+  courses = resource<Course[], unknown>({
+    loader: async () => {
+      return await this.coursesService.loadAllCourses()
+    }
+  })
+
+  /*
+  course = resource({
+    loader: async (params) => {
+      const response = await fetch(`/api/courses/${params.request?.courseId}`);
+      return await response.json();
+    }
+  })
+
+  */
+
+  constructor() {
+
+    effect(() => {
+      console.log("Courses: ", this.courses.value());
+      console.log("Loading:", this.courses.isLoading());
+    })
+
+  }
 
 
 }
